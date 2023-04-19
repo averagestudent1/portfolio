345308a3fc1ea577aed9f59e46be96655cd56fb1
averagestudent1 <studentmail417@gmail.com>
2023-04-19 08:13:25 +0200

    test fetch examples

--- index.js
+++ index.js
@@ -12,3 +12,35 @@ fetch("https://api.github.com/users/averagestudent1/events/public")
     console.log(res);
   })
 
+
+//const fetch = require('node-fetch'); 
+
+const url = 'https://andruxnet-random-famous-quotes.p.rapidapi.com/?cat=movies&count=10';
+
+const options = {
+  method: 'POST',
+  headers: {
+    'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY',
+    'X-RapidAPI-Host': 'andruxnet-random-famous-quotes.p.rapidapi.com'
+  }
+};
+
+fetch(url, options)
+  .then(res => res.json())
+  .then(json => {
+    console.log(json)
+  }
+  )
+  .catch(err => console.error('error:' + err));
+
+
+//test
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+  .then(response => response.json())
+  .then(json => console.log(json))
+
+// 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
+
+// TODO 1. Fetch info from GIT
+// TODO 2. Parse response
+// TODO 3. Display an HTML template
