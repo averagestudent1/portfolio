db3a46679f1703bf1129fae60d2f7d03863898d3
averagestudent1 <studentmail417@gmail.com>
2023-04-19 08:10:23 +0200

    fetch from github api render activity

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Hello from index.js")
+fetch("https://api.github.com/users/averagestudent1/events/public")
+  .then(res => res.json())
+  .then(res => {
+
+    const section = document.createElement("section")
+    res.forEach(githubEvent => {
+      const div = document.createElement("div")
+      div.innerHTML = `eventtype: ${githubEvent.type}`
+      section.appendChild(div)
+    });
+    document.getElementById("githubresult").appendChild(section)
+    console.log(res);
+  })
+
